875d91d6e84667e1b5e53f5b303e70f5d58459f5
Конкин Иван <konkin.ivan@yandex.ru>
2023-01-27 10:13:13 +0300

    My Custom MVC on php

---
+++ web/public/config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+  'database' => [
+    'host' => 'db',
+    'port' => 3306,
+    'dbname' => 'posts',
+    'charset' => 'utf8'
+  ]
+  
+//  'services' => [
+//    'prerender' => [
+//      'token' => '',
+//      'secret' => ''
+//    ]
+//  ]
+];
+
--- web/public/index.php
+++ web/public/index.php
@@ -1,5 +1,20 @@
 <?php
 
 require_once 'functions.php';
-
+require_once 'Database.php';
 require_once 'router.php';
+//phpinfo();
+
+$config = require_once 'config.php';
+
+$db = new Database($config['database']);
+
+$posts = $db->query("select * from posts")->fetchAll();
+
+foreach ($posts as $post) {
+  echo "<li>" . $post['Title'] . "</li>";
+}
+
+echo '<pre>';
+//var_dump($posts);
+echo '</pre>';
